7635bc33ca0c853db6efc585dc25b9c02c12046b
FUENTES SUAREZ Alvaro Javier <alvaro.fuentes-suarez@inria.fr>
2019-08-02 11:45:51 +0200

    documented base.h

--- src/base.h
+++ src/base.h
@@ -1,3 +1,6 @@
+/** @file base.h Base include for all includes in this project.
+ */
+
 #ifndef BASE_H
 #define BASE_H
 
@@ -15,33 +18,94 @@
 
 #include <eigen3/Eigen/Dense>
 
-//Constants
+/** Tolerance used for most comparisons.
+ */
 #define TOL 1e-10
+
+/** Value of pi with high precision.
+ */
 #define PI_ 3.14159265358979323846
 
-//Predefinitions
-// class Skeleton;
-// class Field;
-
-//Linear Algebra
+/**
+ * @brief      Point in 3D.
+ *
+ * @see        Vector
+ * @see        UnitVector
+ */
 typedef Eigen::Vector3d Point;
+/**
+ * @brief      Vector in 3D.
+ *
+ *             Currently it is the same as Point but the implementation may
+ *             evolve.
+ *
+ * @see        Point
+ * @see        UnitVector
+ */
 typedef Eigen::Vector3d Vector;
+/**
+ * @brief      Unit vector (unit norm) in 3D.
+ *
+ *             Currently is the same as Vector, but in the future the unit norm
+ *             may be enforced.
+ *
+ * @see        Vector
+ * @see        Point
+ */
 typedef Eigen::Vector3d UnitVector;
+
+/**
+ * @brief      3x3 matrix.
+ *
+ * @see        Frame
+ */
 typedef Eigen::Matrix3d Matrix;
+/**
+ * @brief      Orthonormal matrix.
+ *
+ *             Currently is the same as Matrix, future implementation may
+ *             enforce orthonormal property.
+ *
+ * @see        Matrix
+ */
 typedef Eigen::Matrix3d Frame;
 
-
+/**
+ * @brief      Stores points without duplications and returns their indexes.
+ */
 class PointIndexer
 {
 public:
+    /**
+     * @brief      Gets the points currently stored.
+     *
+     * @return     The vector of points.
+     */
     const std::vector<Point>& get_points() const
     {
         return points;
     }
+
+    /**
+     * @brief      Gives the number of points currently stored.
+     *
+     * @return     The size of the current vector of points.
+     * @see        get_points()
+     */
     int size() const
     {
         return points.size();
     }
+
+    /**
+     * @brief      Indexes a point within the stored points. It adds the point
+     *             to the storage if it is not on it already.
+     *
+     * @param[in]  p     The point to index.
+     *
+     * @return     The index (position) within the storage.
+     * @see        get_points()
+     */
     int index(const Point& p)
     {
         if(point_idxs.find(p)==point_idxs.end())
@@ -53,6 +117,13 @@ public:
     }
 
 private:
+
+    /**
+     * @brief      Internal implementation of comparison for points.
+     *
+     *             Two points are compared lexicographically within a tolerance
+     *             given by the macro TOL.
+     */
     struct ComparePoints
     {
         bool operator() (const Point& a, const Point& b) const
